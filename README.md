a17f25f2f1ffad681d96c24e2fc5b9bc9c6dee35
Vishal <vishal_agrawal@ymail.com>
2016-10-13 15:57:10 +0200

    Added mock_mode
    mock_mode will use markovify to do markov chaining on user’s tweet
    history. therefore it will try to mock the user.

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ Install tweepy by running `$ pip install tweepy`
 
 More about [tweepy](http://www.tweepy.org/)
 
+Install markovify by running `$ pip install markovify`
+
+More about [markovify]()
+
 ### credentials.py
 In order to read or write from the Twitter account, you need to authenticate.
 
@@ -22,10 +26,13 @@ Fill in the appropriate values from the Keys and Access Tokens page inside the q
 ### reply_list.txt
 Here you maintain the list of replies separated by new lines. This will reload before ever reply so you can dynamically change the contents.
 
-### main.py
-Call using twitter handle (without @)
+### user_tweet_history.txt (Optional)
+Here you would have the tweet history of the user, used if you are in mock mode.
 
-Example: `$ python3 main.py realDonaldTrump`
+### main.py
+Call using twitter handle (without @) and optional 0 or 1 for mock mode. 1 is on, 0 is off. If last argument isn't specified mock mode is by default off
+
+Example: `$ python3 main.py realDonaldTrump 1`
 
 ## Considerations
 - If the user you are auto-replying to tweets in rapid succession, the loading of *reply_list.txt* may slow down performance. 
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import sys
 from time import ctime
 from tweepy import API
@@ -7,26 +6,23 @@ from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 from credentials import *
 from tweepy.utils import import_simplejson
+import markovify
 import random
+
 json = import_simplejson()
 
 
 class Listener(StreamListener):
-    def __init__(self, api, followed_user_id, followed_user_handle):
+    def __init__(self, api, followed_user_id, followed_user_handle, mock_mode):
         super().__init__(api)
         self.tweet_data = []
         self.followed_user_id = followed_user_id
         self.followed_user_handle = followed_user_handle
 
+        self.mock_mode = mock_mode
         self.reply_list = []
         self.next_reply = ""
-        self.load_replies()
-
-    def load_replies(self):
-        with open('reply_list.txt', 'r') as reply_list_file:
-            self.reply_list = reply_list_file.readlines()
-
-        self.next_reply = random.choice(self.reply_list)
+        self.load_next_reply(mock_mode)
 
     def on_error(self, error):
         print("Returned error code %s" % error)
@@ -37,15 +33,42 @@ class Listener(StreamListener):
             tweet_text = '@%s %s' % (self.followed_user_handle, self.next_reply)
             self.api.update_status(tweet_text)
 
-            self.load_replies()
             print("%s: Tweeted:" % (ctime(), tweet_text))
 
+            if self.mock_mode:
+                self.update_mock_text(status.text)
+
+            self.load_next_reply(self.mock_mode)
+
+    def load_next_reply(self, mock=False):
+        if not mock:
+            with open('reply_list.txt', 'r') as reply_list_file:
+                self.reply_list = reply_list_file.readlines()
+
+            self.next_reply = random.choice(self.reply_list)
+
+        else:
+            with open("user_tweet_history.txt") as user_tweet_history_file:
+                text = user_tweet_history_file.read()
+
+            text_model = markovify.Text(text)
+            self.next_reply = text_model.make_short_sentence(140)
+
+    @staticmethod
+    def update_mock_text(text):
+        with open('user_tweet_history.txt', 'wa') as user_tweet_history_file:
+            user_tweet_history_file.write(text)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    args = len(sys.argv)
+    if args < 2:
         print('Need twitter handle (without @).')
         sys.exit()
+
+    mock_mode = False
+    if args == 3:
+        mock_mode = sys.argv[2] == "1" or sys.argv[2].lower() == "true"
 
     followed_user_handle = sys.argv[1]
 
@@ -61,6 +84,5 @@ if __name__ == "__main__":
 
     followed_user_id = found_users[0].id
 
-    twitterStream = Stream(auth, Listener(api, followed_user_id, followed_user_handle))
+    twitterStream = Stream(auth, Listener(api, followed_user_id, followed_user_handle, mock_mode))
     twitterStream.filter(follow=[str(followed_user_id)], async=True)
-
